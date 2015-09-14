9344c159ba3af2c8d943e80e4c40a97b1bc63268
Rene Schulte <contact@rene-schulte.info>
2015-09-14 13:14:17 +0200

    Added rotating cube

---
+++ Unity5Test/Assets/Scripts/TransformAnimBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformAnimBehavior : MonoBehaviour
+{
+    public Vector3 LocalRotateEulerDelta = Vector3.zero;
+    public bool ShouldRotateRelativeToGlobal = false;
+
+    private void Update()
+    {
+        transform.Rotate(LocalRotateEulerDelta.x, LocalRotateEulerDelta.y, LocalRotateEulerDelta.z, ShouldRotateRelativeToGlobal ? Space.World : Space.Self);
+    }
+}
